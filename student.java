5d5f32c06e687194e4a4062a60366e934f1282f8
ichaxpro <dewiicha161@gmail.com>
2025-04-16 16:38:54 +0700

    Case Study Jobsheet 1

---
+++ student.java
@@ -0,0 +1,16 @@
+package caseStudyJS1;
+public class student {
+  String NIM, name, prodi; 
+
+  public student(String NIM, String name, String prodi) {
+      this.NIM = NIM;
+      this.name = name;
+      this.prodi = prodi;
+  }
+
+  public void display() {
+      System.out.println("NIM : " + NIM + " | Name : " + name + " | Prodi : " + prodi);
+}
+
+
+}
